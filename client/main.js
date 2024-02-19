459d03bad1420e6218d389945b1086102d377f19
David Louis <davloufl@gmail.com>
2024-02-19 00:55:15 -0600

    Waited till the last second again... Will do better this friday

--- client/main.js
+++ client/main.js
@@ -1,4 +1,9 @@
 const complimentBtn = document.getElementById("complimentButton")
+const fortuneBtn = document.getElementById("fortuneButton")
+const submit = document.getElementById("submit")
+
+const createCharacter = body => axios.post("http://localhost:4000/api/characters/", body)
+
 
 const getCompliment = () => {
     axios.get("http://localhost:4000/api/compliment/")
@@ -8,4 +13,56 @@ const getCompliment = () => {
     });
 };
 
+const getFortune = () => {
+    axios.get("http://localhost:4000/api/fortune/").then(res => {
+            const data = res.data
+            alert(data)
+        })
+}
+
+const checkButton = () => {
+    axios.get("http://localhost:4000/api/game/").then(res => {
+        const data = res.data
+        alert(data)
+    })
+}
+
+function submitHandler(e) {
+    e.preventDefault()
+
+    let cname = document.querySelector("#cname")
+    let str = document.querySelector("#str")
+    let dex = document.querySelector("#dex")
+    let consti = document.querySelector("#consti")
+    let int = document.querySelector("#int")
+    let wis = document.querySelector("#wis")
+    let char = document.querySelector("#char")
+    let comli = document.querySelector("#comli")
+
+    let bodyObj = {
+        cname: cname.value,
+        str: str.value,
+        dex: dex.value,
+        consti: consti.value,
+        int: int.value,
+        wis: wis.value,
+        char: char.value,
+        comli: comli.value
+    }
+
+    createCharacter(bodyObj)
+
+    cname.value = ''
+    str.value = 0
+    dex.value = 0
+    consti.value = 0
+    int.value = 0
+    wis.value = 0
+    char.value = 0
+    comli.value = 0
+
+}
+
+fortuneBtn.addEventListener('click', getFortune)
 complimentBtn.addEventListener('click', getCompliment)
+submit.addEventListener('click', submitHandler)
--- server/controller.js
+++ server/controller.js
@@ -1,3 +1,6 @@
+const characters = require("./db.json")
+
+
 module.exports = {
 
     getCompliment: (req, res) => {
@@ -8,6 +11,33 @@ module.exports = {
         let randomCompliment = compliments[randomIndex];
       
         res.status(200).send(randomCompliment);
+    },
+
+    getFortune: (req, res) => {
+        const fortunes = ["You shall receive something nice from a friend.", "You need to watch you back to avoid bad tidings soon.", "The woman/man of your dreams will madly fall in love with you!", "The Spice must flow!", "The sleeper has awakened!"];
+
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+
+        res.status(200).send(randomFortune);
+    },
+
+     logDndCharacter: (req, res) => {
+        let { cname, str, dex, consti, int, wis, char, comli } = req.body
+
+        let newCharacter = {
+            cname,
+            str,
+            dex,
+            consti,
+            int,
+            wis,
+            char,
+            comli
+        }
+
+        characters.push(newCharacter)
+        res.status(200).send(characters)
     }
 
 }
